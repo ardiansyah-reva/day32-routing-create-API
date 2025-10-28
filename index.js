49890c0e630ecf3b44a49c358ac36db1902de992
ardinsyah-reva <revaardiansyahabdullah@gmail.com>
2025-10-28 22:08:41 +0700

    update: perbaikan route & CRUD users

--- index.js
+++ index.js
@@ -14,39 +14,111 @@ app.get("/about", (req, res) => {
   res.send("Hello, nama saya Reva Ardiansyah Abdullah");
 });
 
-app.get("/products", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: "kopi kapal api",
-      price: 2500,
-      stock: 12,
-    },
-    {
-      id: 2,
-      name: "kopi good day",
-      price: 3000,
-      stock: 12,
-    },
-    {
-      id: 3,
-      name: "kopi luwak",
-      price: 2500,
-      stock: 12,
-    },
-    {
-      id: 4,
-      name: "kopi ABC",
-      price: 2000,
-      stock: 12,
-    },
-    {
-      id: 5,
-      name: "kopi liyong",
-      price: 5000,
-      stock: 12,
-    },
-  ]);
+let users = [
+  { id: 1,
+    name: "Udin",
+    NIM: 25101101,
+    studi: "Sarjana Informasi"
+  },
+  { id: 2,
+    name: "Selamet",
+    NIM: 25101102, 
+    studi: "Teknik Informatika" 
+  },
+  { id: 3, 
+    name: "Ganjar", 
+    NIM: 25101103, 
+    studi: "Teknik Industri" 
+  },
+  { id: 4, 
+    name: "Bahlil", 
+    NIM: 25101104, 
+    studi: "Teknik Sipil" 
+  },
+  { id: 5, 
+    name: "Agus", 
+    NIM: 25101105, 
+    studi: "Sarjana Hukum" 
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((p) => p.id === id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+  res.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name || !newUser.NIM || !newUser.studi) {
+    return res.status(400).json({
+      message: "Field 'name', 'NIM' dan 'studi' wajib diisi!",
+    });
+  }
+
+  const lastId = users.length > 0 ? Math.max(...users.map((p) => p.id)) : 0;
+  newUser.id = lastId + 1;
+  users.push(newUser);
+  users.sort((a, b) => a.id - b.id);
+
+  res.status(201).json({
+    message: "User berhasil ditambahkan!",
+    data: newUser,
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedData = req.body;
+  const index = users.findIndex((p) => p.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: `User dengan id ${id} tidak ditemukan!`,
+    });
+  }
+
+  users[index] = { ...users[index], ...updatedData };
+  res.json({
+    message: "Data user berhasil diupdate!",
+    data: users[index],
+  });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id)) {
+    return res.status(400).json({ message: "ID tidak valid" });
+  }
+
+  const index = users.findIndex((p) => p.id === id);
+  if (index === -1) {
+    return res
+      .status(404)
+      .json({ message: `User dengan id ${id} tidak ditemukan!` });
+  }
+
+  const [deleted] = users.splice(index, 1);
+  res.status(200).json({
+    message: `User ${id} berhasil dihapus!`,
+    data: deleted,
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error("Error terdeteksi:", err.message);
+  res.status(500).json({
+    status: "error",
+    message: "Terjadi kesalahan internal pada server.",
+    detail: err.message,
+  });
 });
 
 app.listen(PORT, () => {
